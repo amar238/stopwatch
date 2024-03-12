fc6d57314452ade4eafa197189d4878b0ac4b9fc
amar238 <amargurav238@gmail.com>
2024-03-12 13:18:55 +0530

    rearrange and comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var resetBtn = document.getElementById("resetBtn");
 var timer;
 var hours = 0, minutes = 0, seconds = 0 , miliseconds=0;
 
+// start/resume stopwatch
 function startStopwatch() {
     startBtn.style.backgroundColor = "white";
     stopBtn.style.backgroundColor = "red";
@@ -14,6 +15,7 @@ function startStopwatch() {
     clearInterval(timer);
     timer = setInterval(updateStopwatch, 10);
 }
+// pause stopwatch
 function stopStopwatch() {
     clearInterval(timer);
     startBtn.style.backgroundColor = "greenyellow";
@@ -22,6 +24,7 @@ function stopStopwatch() {
     resetBtn.style.backgroundColor = "orange"
     resetBtn.style.color="blue"
 }
+// reset stopwatch clear the values of time variables
 function resetStopwatch() {
     clearInterval(timer);
     startBtn.style.backgroundColor = "greenyellow";
@@ -29,9 +32,10 @@ function resetStopwatch() {
     stopBtn.style.color = "black"
     resetBtn.style.backgroundColor = "white"
     resetBtn.style.color="black"
-    hours = minutes = seconds = 0;
+    hours = minutes = seconds = miliseconds = 0;
     stopwatchElement.textContent = "hh:mm:ss:ms\n00:00:00:00";
 }
+// update display for every 10 miliseconds
 function updateStopwatch() {
     miliseconds+=10;
     if(miliseconds>=1000){
@@ -48,6 +52,7 @@ function updateStopwatch() {
     }
     stopwatchElement.textContent = "hh:mm:ss:ms\n" + arrange(hours) + ":" + arrange(minutes) + ":" + arrange(seconds) + ":" + arrangeMS(miliseconds);
 }
+// functions to add 0s so that structure on display remains intact
 function arrange(num) {
   return (num < 10 ? "0" : "") + num;
 }
